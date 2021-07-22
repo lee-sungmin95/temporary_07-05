2a06e6e27956cdd3f4c730a61eda3af591e62276
95sungmin <dltjdals1929@gmail.com>
2021-07-22 17:47:00 +0900

    07/22 2ban commit 3

--- g0705/settings.py
+++ g0705/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'accountapp'
+    'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
